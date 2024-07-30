468a52eca798b6bf497572daeab3cca1d6db1ec0
leonidafanas <117893247+leonidafanas@users.noreply.github.com>
2024-07-30 15:04:00 -0500

    first commit

---
+++ scripts/deployFLashLoan.js
@@ -0,0 +1,16 @@
+const hre = require("hardhat");
+
+async function main() {
+  const FlashLoan = await hre.ethers.getContractFactory("FlashLoan");
+  const flashLoan = await FlashLoan.deploy(
+    "0x012bAC54348C0E635dCAc9D5FB99f06F24136C9A"
+  );
+
+  await flashLoan.waitForDeployment();
+  console.log("Flash loan contract deployed:", flashLoan.target);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
